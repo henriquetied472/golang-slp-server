4c42ebd95605dfbbd123a2ce4698463bef6ee7c1
Henrique Goulart <hgoulart659@gmail.com>
2026-06-30 20:27:50 -0300

    basic auth implementation

--- auth.go
+++ auth.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"crypto/sha1"
+)
+
+func SHA1[T string | []byte](data T) []byte {
+	raw := []byte(data)
+	
+	sum := sha1.Sum(raw)
+	return sum[:]
+}
+
 type AuthProvider interface {
 	Verify(username string, chalenge []byte, response []byte) bool
 }
+
